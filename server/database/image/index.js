2d2661ab397a9eb9adb6fc1cb65b056988c1597a
ShitalKarhale <shitalkarhale7@gmail.com>
2021-08-23 14:02:58 +0530

    Schema added

---
+++ server/database/image/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const ImageModel=new mongoose.Schema({
+
+    images:[
+        {
+          location:{type:String,required:true},
+        },
+
+    ],
+});
+export const ImageModel=mongoose.model("Images",ImageSchema);
